70aab4124aec31298ca022c5df6648a7df054d7e
Lorenzo Fusaro <lorenzofusaro@email.arizona.edu>
2014-04-12 11:27:20 -0700

    stuff

--- FinalProject/src/Unit.java
+++ FinalProject/src/Unit.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 
-public abstract class Unit {
+
+public abstract class Unit extends GameObject{
 		
 	private String name;
 	private int health;
 	private int moveDistance;
+	private ArrayList <Item> items;
+	private int [] stats;
+	private Strategy strategy;
 	
 	public Unit (String n, int hp, int distance){
 		this.name = n;
@@ -21,6 +26,38 @@ public abstract class Unit {
 	
 	public int getDistance(){
 		return moveDistance;
+	}
+	
+	public int takeHit(int damage){
+		health=health-damage;
+		if (health<0){
+			dead();
+		}
+	}
+
+	private void dead() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void addItem (Item item){
+		items.add(item);
+	}
+	
+	public ArrayList getInventory(){
+		return items;
+	}
+	
+	public void trade(Unit u, Item I){
+		u.addItem(I);
+	}
+	
+	public void removeItem(Item I){
+		for(Item obj: items){
+			if(I.compareTo(obj)==0){
+				items.remove();
+			}
+		}
 	}
 	
 }
