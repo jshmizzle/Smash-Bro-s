76984cac1b5db2f14aa862bf4f83ea36891ac0f5
Lorenzo Fusaro <lorenzofusaro@email.arizona.edu>
2014-04-19 18:19:41 -0700

    yay

---
+++ FinalProject/src/client/ComputerServerHandler.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import command.Command;
+
+public class ComputerServerHandler implements Runnable{
+	ObjectInputStream inputStream;
+	ComputerClient client;
+	Command<ComputerClient> command;
+	
+	public ComputerServerHandler(ComputerClient client, ObjectInputStream in){
+		this.client=client;
+		this.inputStream=in;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void run() {
+		while(true){
+			try {
+				this.command=(Command<ComputerClient>)inputStream.readObject();
+				command.execute(client);
+			} catch (ClassNotFoundException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
