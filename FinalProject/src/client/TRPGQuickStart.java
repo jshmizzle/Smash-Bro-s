e0878ddff4b519235b33322800d691d4df72d702
Jason Steving <jasonsteving@email.arizona.edu>
2014-04-26 16:00:54 -0700

    made a quickstart class

---
+++ FinalProject/src/client/TRPGQuickStart.java
@@ -0,0 +1,16 @@
+package client;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import server.TRPGServer;
+
+public class TRPGQuickStart {
+
+	public static void main(String[] args) {
+		TRPGServer server=new TRPGServer(9001);
+		ComputerClient compClient=new ComputerClient();
+		TRPGClient userClient=new TRPGClient("localhost", 9001, "User");
+	}
+}
+
